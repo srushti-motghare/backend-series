bb20515f164dafb4634e3cab47953f92d319a1ea
srushti-motghare <srushtimotghare325@gmail.com>
2024-06-14 14:37:52 +0530

    add connetivity code and also added utilies

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './env' });
+
+import express from 'express';
+import connectDB from './db/index.js';  // Ensure the correct import path
+
+const app = express();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MONGO db connection failed !!!', err);
+  });
+
+/*
+
+import express from "express"
+const app=express()
+
+(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+          console.log("ERRR:",error);
+          throw error
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port${process.env.PORT}`);
+       })
+    }catch(error){
+        console.error("ERROR:", error)
+        throw err
+    }
+})()
+
+*/
